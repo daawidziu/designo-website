e090d1461f82a6c9bb8336c008fce25f782bc953
daawidziu <daawidziu@int.pl>
2022-01-14 22:17:07 +0100

    Create CardLocation component

---
+++ src/components/locations/_CardLocation.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import styles from "../../styles/components/locations/CardLocation.module.sass";
+
+interface Props {
+    image: string
+    country: string
+    office: string
+    street: string
+    state: string
+    number: string
+    email: string
+}
+
+export function CardLocation({image, country, office, street, state, number, email}: Props) {
+    return (
+        <li className={styles.card} key={office}>
+            <div className={styles.image}>
+                <Image
+                    quality={100}
+                    alt="location image"
+                    src={image}
+                    layout='fill'
+                    objectFit="cover"
+                />
+            </div>
+            <address className={styles.info}>
+                <h2>{country}</h2>
+                <div className={styles.specifics}>
+                    <div>
+                        <h3>{office}</h3>
+                        <span>{street}</span>
+                        <span>{state}</span>
+                    </div>
+                    <div>
+                        <h3>Contact</h3>
+                        <span>P: {number}</span>
+                        <span>M: {email}</span>
+                    </div>
+                </div>
+            </address>
+        </li>
+    );
+}
